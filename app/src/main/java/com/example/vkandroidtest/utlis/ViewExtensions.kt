584480222954c608942bff38d04e29e4f4a3ed4c
Kot512 <kelohov512@gmail.com>
2024-03-12 21:29:51 +0300

    Fixed image aspect ratio

--- app/src/main/java/com/example/vkandroidtest/utlis/ViewExtensions.kt
+++ app/src/main/java/com/example/vkandroidtest/utlis/ViewExtensions.kt
@@ -1,6 +1,5 @@
 package com.example.vkandroidtest.utlis
 
-import android.content.Context
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.example.vkandroidtest.R
@@ -10,7 +9,6 @@ object ViewExtensions {
     fun ImageView.loadFromUrl(url: String) {
         Glide.with(this)
             .load(url)
-            .centerCrop()
             .placeholder(R.drawable.ic_launcher_foreground)
             .into(this)
     }
